8dc37771782485c76235521d8c5389d0dd603fbe
DimaMen265 <menzak87@gmail.com>
2024-01-30 14:38:27 +0100

    The end tasks

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -3,7 +3,9 @@ import React, { useState } from 'react';
 export function FormComponent() {
   const [value, setValue] = useState("");
 
-  const handleChange = (event) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
     setValue(event.target.value);
   };
 
